fb9c4d5e41b79d970e9d16f333c52b8beef03cad
isascarc <isascarch@gmail.com>
2023-08-08 13:51:33 +0300

    Readability

--- TestApi/UnitTest1.cs
+++ TestApi/UnitTest1.cs
@@ -1,41 +1,41 @@
 using EdgeExtensionsApi;
-namespace TestApi
+
+namespace TestApi;
+
+public class Tests
 {
-    public class Tests
+    private const string CrxId1 = "eeagobfjdenkkddmbclomhiblgggliao";
+
+    [SetUp]
+    public void Setup()
     {
-        private const string CrxId1 = "eeagobfjdenkkddmbclomhiblgggliao";
+    }
 
-        [SetUp]
-        public void Setup()
-        {
-        }
+    [Test]
+    public void Test1()
+    {
+        Assert.ThrowsAsync<ArgumentNullException>(() => Extensions.GetExtensionDetail(null));
+        Assert.ThrowsAsync<ArgumentException>(() => Extensions.GetExtensionDetail(""))
 
-        [Test]
-        public void Test1()
-        {
-            Assert.ThrowsAsync<ArgumentNullException>(() => Extensions.GetExtensionDetail(null));
-            Assert.ThrowsAsync<ArgumentException>(() => Extensions.GetExtensionDetail(""));
+        var t = Extensions.GetExtensionDetail(CrxId1).Result;
+        Assert.That(t, Is.Not.Null);
+    }
 
-            var t = Extensions.GetExtensionDetail(CrxId1).Result;
-            Assert.That(t, Is.Not.Null);
-        }
+    [Test]
+    public static void GetSuggestionsTest()
+    {
+        Assert.That(Extensions.GetSuggestions("pdf").Result, Is.Not.Null);
 
-        [Test]
-        public static void GetSuggestionsTest()
-        {
-            Assert.That(Extensions.GetSuggestions("pdf").Result, Is.Not.Null);
+        Assert.ThrowsAsync<ArgumentNullException>(() => Extensions.GetSuggestions(null));
+        Assert.ThrowsAsync<ArgumentException>(() => Extensions.GetSuggestions(""));
+    }
 
-            Assert.ThrowsAsync<ArgumentNullException>(() => Extensions.GetSuggestions(null));
-            Assert.ThrowsAsync<ArgumentException>(() => Extensions.GetSuggestions(""));
-        }
+    [Test]
+    public static void SearchTest()
+    {
+        Assert.That(Extensions.Search("pdf").Result, Is.Not.Null);
 
-        [Test]
-        public static void SearchTest()
-        {
-            Assert.That(Extensions.Search("pdf").Result, Is.Not.Null);
-
-            Assert.ThrowsAsync<ArgumentNullException>(() => Extensions.Search(null));
-            Assert.ThrowsAsync<ArgumentException>(() => Extensions.Search(""));
-        }
+        Assert.ThrowsAsync<ArgumentNullException>(() => Extensions.Search(null));
+        Assert.ThrowsAsync<ArgumentException>(() => Extensions.Search(""));
     }
 }
